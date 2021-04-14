4f112fa9cc5606465e0b438be70d85033caad0b1
Paul Bouhours <bouhours.paul@gmail.com>
2021-04-14 11:38:40 +0200

    stylisation du composent

--- src/components/HerbariumCard.jsx
+++ src/components/HerbariumCard.jsx
@@ -4,18 +4,22 @@ import './HerbariumCard.css';
 const magnolia = {
   image:
     'https://www.meillandrichardier.com/media/catalog/product/cache/1/image/800x800/040ec09b1e35df139433887a97daa66f/8/0/8072a-magnolia_susan.jpg',
-  gender: 'magolitumsarasin',
-  location: 'jardin des plantes',
+  gender: 'Magnolia des tropiques ',
+  location: 'Parc de la Chanterie',
 };
 function HerbariumCard() {
   return (
     <div className="cardsHerbarium">
       <img src={magnolia.image} alt="photoMagnolia" className="photoMagnolia" />
       <div className="infoCard">
-        <h3 className="gender card">Genre</h3>
-        <p className="content gender">{magnolia.gender}</p>
-        <h3 className="location card">Localisation</h3>
-        <p className="content location">{magnolia.location}</p>
+        <div className="gender">
+          <h3 className="title">Genre</h3>
+          <p className="content">{magnolia.gender}</p>
+        </div>
+        <div className="location">
+          <h3 className="title">Localisation</h3>
+          <p className="content">{magnolia.location}</p>
+        </div>
       </div>
       <img
         src="https://cdn.discordapp.com/attachments/816732696658313229/831525323622318190/magnolia-icon-green.png"
