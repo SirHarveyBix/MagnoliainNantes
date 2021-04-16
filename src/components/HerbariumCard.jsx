ce8e68aecddf6b6d69417398e4a93319d7423fdc
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-04-16 11:07:38 +0200

    fixes

--- src/components/HerbariumCard.jsx
+++ src/components/HerbariumCard.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import './HerbariumCard.css';
 
