e964e04cd4a5ed4bcee63130c26a1932720004f3
Victor Gaugler <72219421+vgaugler@users.noreply.github.com>
2021-04-26 12:22:37 +0200

    resolve comment pull request

--- src/components/mapProgress/ProgressBar.jsx
+++ src/components/mapProgress/ProgressBar.jsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import './ProgressBar.css';
 
-function ProgressBar({ largeur, progress }) {
-  const largeurProgress = progress * largeur;
-  const porcent = Math.floor(progress * 100);
+function ProgressBar({ width, progress }) {
+  const largeurProgress = progress * width;
+  const percent = Math.floor(progress * 100);
   return (
-    <div className="progress-div" style={{ width: largeur }}>
+    <div className="progress-div" style={{ width: width }}>
       <div style={{ width: `${largeurProgress}px` }} className="progress">
-        <p>{porcent}%</p>
+        <p>{percent}%</p>
       </div>
     </div>
   );
